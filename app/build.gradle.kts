d86cfea5ea06db331881abf03a3af806f797414f
Laury PRIN <prinlauryy@gmail.com>
2025-11-27 15:06:34 +0100

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.openclassroom.p8_vitesse"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.openclassroom.p8_vitesse"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Room
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    implementation(libs.androidx.activity)
+    ksp(libs.room.compiler)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.nav.fragment)
+    ksp(libs.hilt.compiler)
+
+    // AndroidX
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintLayout)
+    implementation(libs.activity.ktx)
+    implementation(libs.fragment.ktx)
+
+    // Desugaring
+    coreLibraryDesugaring(libs.desugar)
+
+    // Tests
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.espresso.core)
+    testImplementation(libs.mockk)
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.turbine)
+}
---
+++ app/src/main/java/com/openclassroom/p8_vitesse/VitesseApplication.kt
@@ -0,0 +1,7 @@
+package com.openclassroom.p8_vitesse
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class VitesseApplication : Application()
