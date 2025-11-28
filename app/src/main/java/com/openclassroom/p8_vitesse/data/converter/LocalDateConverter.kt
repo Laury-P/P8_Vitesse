5cb4f4321ce8c7694d98e5c3fed7bc5aa78df6cc
Laury PRIN <prinlauryy@gmail.com>
2025-11-28 10:34:34 +0100

    création du type converter

---
+++ app/src/main/java/com/openclassroom/p8_vitesse/data/converter/LocalDateConverter.kt
@@ -0,0 +1,16 @@
+package com.openclassroom.p8_vitesse.data.converter
+
+import androidx.room.TypeConverter
+import java.time.LocalDate
+
+class LocalDateConverter {
+    @TypeConverter
+    fun localDateToTimeStamp(date: LocalDate?) : Long? {
+        return date?.toEpochDay()
+    }
+
+    @TypeConverter
+    fun timeStampToLocalDate(value: Long?) : LocalDate? {
+        return value?.let { LocalDate.ofEpochDay(it) }
+    }
+}
