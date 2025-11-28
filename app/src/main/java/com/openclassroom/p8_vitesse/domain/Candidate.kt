01398ea35cb6ae5b82b4fedaac9c41ee763d186b
Laury PRIN <prinlauryy@gmail.com>
2025-11-28 10:22:07 +0100

    Model de donnée pour candiate

---
+++ app/src/main/java/com/openclassroom/p8_vitesse/domain/Candidate.kt
@@ -0,0 +1,16 @@
+package com.openclassroom.p8_vitesse.domain
+
+import java.time.LocalDate
+
+data class Candidate(
+    val id: Long? = null,
+    var photo: String? = null,
+    var firstName: String,
+    var lastName: String,
+    var note: String? = null,
+    var isFavorite: Boolean = false,
+    var phoneNumber: String,
+    var email: String,
+    var dateOfBirth: LocalDate,
+    var expectedSalary: Double? = null,
+)
