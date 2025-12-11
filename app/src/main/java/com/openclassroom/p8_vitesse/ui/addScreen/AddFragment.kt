bdc619c4314ebf13898590a337188a4eb9907758
Laury PRIN <prinlauryy@gmail.com>
2025-12-11 16:15:53 +0100

    Ajout de l'onglet photo du candidat fonctionnel avec selection d'une photo dans la galerie

--- app/src/main/java/com/openclassroom/p8_vitesse/ui/addScreen/AddFragment.kt
+++ app/src/main/java/com/openclassroom/p8_vitesse/ui/addScreen/AddFragment.kt
@@ -1,5 +1,6 @@
 package com.openclassroom.p8_vitesse.ui.addScreen
 
+import android.net.Uri
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,10 +8,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.openclassroom.p8_vitesse.R
 import com.openclassroom.p8_vitesse.databinding.FragmentAddBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.net.URI
 
 @AndroidEntryPoint
 class AddFragment : Fragment(R.layout.fragment_add) {
@@ -18,8 +22,14 @@ class AddFragment : Fragment(R.layout.fragment_add) {
     private var _binding: FragmentAddBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel : AddViewModel by viewModels()
+    private val viewModel: AddViewModel by viewModels()
 
+    private val pickMedia =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                setProfilPicture(uri)
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,12 +43,31 @@ class AddFragment : Fragment(R.layout.fragment_add) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         goBack()
+        setImagePicker()
     }
 
-    fun goBack(){
+    private fun goBack() {
         binding.toolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
         }
     }
+
+    /**
+     * Lancement de pickMedia pour que l'utilisateur puissent choisir la photo a utiliser pour le candidat
+     */
+    private fun setImagePicker(){
+        binding.profilPicture.setOnClickListener{
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+    }
+
+    /**
+     * Mise à jour de la photo de profil
+     */
+    private fun setProfilPicture(uri: Uri) {
+        viewModel.setPhoto(uri.toString())
+        binding.profilPicture.setImageURI(uri)
+    }
+
 
 }
--- app/src/main/java/com/openclassroom/p8_vitesse/ui/addScreen/AddViewModel.kt
+++ app/src/main/java/com/openclassroom/p8_vitesse/ui/addScreen/AddViewModel.kt
@@ -1,7 +1,25 @@
 package com.openclassroom.p8_vitesse.ui.addScreen
 
 import androidx.lifecycle.ViewModel
+import com.openclassroom.p8_vitesse.data.repository.CandidateRepository
+import com.openclassroom.p8_vitesse.domain.Candidate
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.LocalDate
+import javax.inject.Inject
 
-class AddViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class AddViewModel @Inject constructor(private val repository: CandidateRepository) : ViewModel() {
+    private var candidate = Candidate(
+        firstName = " ",
+        lastName = " ",
+        phoneNumber = " ",
+        dateOfBirth = LocalDate.now(),
+        email = " "
+    )
+
+    fun setPhoto(newURI: String){
+        candidate.photo = newURI
+    }
+
+
 }
